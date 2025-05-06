1cdab2eb28ba0f1f85a9d5bb0b187808dc5b5e5f
antebrinken@live.se <antebrinken@live.se>
2025-05-06 10:09:48 +0200

    Created components map and fixed TodoItems

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,22 @@
+interface Todo {
+    id: number;
+    text:string;
+    completed: boolean;
+
+}
+
+interface Props {
+    todo: Todo;
+    onToggle: (id: number) => void;
+    onDelete: (id:number) => void;
+}
+
+export default function TodoItem ({ todo, onToggle, onDelete }: Props) {
+    return (
+        <li style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            {todo.text}
+            <button onClick={() => onToggle.(todo.id)}>Klar</button>
+            <button onClick={() => onDelete(todo.id)}>Ta bort</button>
+            </li>
+    );
+}
