b8d2d39838fbe2209fccd450fcbbe46adbb7c65a
antebrinken@live.se <antebrinken@live.se>
2025-05-06 09:25:53 +0200

    Setting up todo basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
 }
 
-export default App
+function app() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if [newTodo, setNewTodo] = useState('');
+
+    const addTodo = () => {
+      if (newTodo.trim() === '') return;
+
+      const newItem: Todo = {
+        id: Date.now(),
+        text: newTodo,
+        completed: false
+      };
+      
+      setTodos ([...todos, newItem]);
+      setNewTodo('');
+  
+    };
+
+     const toggleTodo = (id: number) => {
+      setTodos(todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ));
+     };
+
+     const deleteTodo = (id: number) => {
+      setTodos(todos.filter(todo => todo.id !== id));
+     };
+
+     return (
+      <div className="app">
+        <h1> Philips To-Do Lista</h1>
+        <div>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={e => setNewTodo(e.target.value)}
+            placeholder="What Todo..."
+          />
+          <button onClick={addTodo}>Lägg till</button>
+        </div>
+        <ul>
+          {todos.map(todo => (
+            <li
+            key={todo.id}
+            style={{textDecoration: todo.completed ? 'line-through' : 'none' }}
+  
+            >
+            {todo.text}
+            <button onClick={() => toggleTodo(todo.id)}>✔</button>
+            <button onClick={() => deleteTodo(todo.id)}>🗑</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
